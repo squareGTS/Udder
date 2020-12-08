725d7bc9102d5fff7381e081243f97cc379e7fbb
squareGTS <squareGTS@icloud.com>
2020-12-08 18:39:41 +0300

    Add label and images.

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Udder")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .padding()
+            Text("Fresh Milk, On Demand.")
+        }
     }
 }
 
